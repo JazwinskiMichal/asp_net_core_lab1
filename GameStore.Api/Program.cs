ad514cf919b5a775dc41397a1755ceaaf6dbb171
Michal Jazwinski <mail@mihow.pl>
2025-05-30 14:12:00 +0200

    Update: added entity framework and migrations

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,19 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("GameStore") ??
+                       throw new InvalidOperationException("Connection string 'GameStore' not found.");
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
 var app = builder.Build();
 
 // Games endpoints
 app.MapGamesEndpoints();
+
+// Apply migrations
+app.MigrateDb();
 
 app.Run();
